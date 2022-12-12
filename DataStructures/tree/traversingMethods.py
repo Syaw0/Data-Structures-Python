5e980b998d357d33769edc3a41e1de7273c908cb
Siavash <siamhb7@protonmail.com>
2022-12-12 18:39:58 +0330

    [Tree] Ds (traversing method) preordering

--- DataStructures/tree/traversingMethods.py
+++ DataStructures/tree/traversingMethods.py
@@ -43,6 +43,23 @@ class Tree:
       self.inOrderTraverse(root.right)
 
 
+  def preOrderTraverse(self, root=0):
+    if root == 0:
+      root = self.tree[0]
+    
+  
+
+    self.tmp.append(root.value)
+
+    if root.left != None:
+      print('this is left',root.left.value)
+      self.preOrderTraverse(root.left)
+    
+    if root.right != None:  
+      print('this is right',root.right.value)
+      self.preOrderTraverse(root.right)
+
+
 x = Tree([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15])
-x.inOrderTraverse()
+x.preOrderTraverse()
 print(x.tmp)
