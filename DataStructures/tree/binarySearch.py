02216c1e5ae9d263c2f198de29d5b51a7d06440f
Siavash <siamhb7@protonmail.com>
2022-12-14 13:17:49 +0330

    [Tree] Binary search ds

---
+++ DataStructures/tree/binarySearch.py
@@ -0,0 +1,7 @@
+  # ** binary search is structure of sorted numbers...
+  # ** in this DS each left side of root node has less than root 
+  # ** and each right side of root node has more than root !
+  # TODO we want 2 operation : 1.search  2.insert
+
+class BinarySearch:
+  pass
