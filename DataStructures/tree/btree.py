84c0a2b84786545a35fac6c8bafec01f5f515d42
Siavash <siamhb7@protonmail.com>
2023-02-10 18:05:20 +0330

    write Btree node with some useful methods.

--- DataStructures/tree/btree.py
+++ DataStructures/tree/btree.py
@@ -2,7 +2,27 @@ class BTreeNode :
     def __init__(self,leaf=False):
         self.leaf = False
         self.keys = []
-        self.values =[]
+        self.children =[]
         self.parent = None
+
+    def getLen(self):
+        return len(self.keys)
     
-        
+    def addKey(self,value):
+        if value == None:
+            return False
+        pos = 0
+        while self.keys[pos]<value :
+            pos += 1
+        self.keys.insert(pos,value)
+    
+    def rmKey(self,pos):
+        if pos >= self.getLen():return False
+        self.keys.pop(pos)
+
+    def addChild(self,node,pos):
+        self.children.insert(pos,node)
+        node.parent = self
+
+    def rmChild(self,pos):
+        self.children.pop(pos)
